5c7c786466fe5fa8db97f2af2d54159e84ad5654
Federico Rampazzo <frampone@gmail.com>
2018-06-21 00:55:18 +0100

    Update to latest Joi and fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: 'vue-loader'
       },
       {
         test: /\.css$/,
@@ -20,7 +20,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
